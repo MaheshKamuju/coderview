aab83a83f6797172ae2b78c4e693fa642a7788d2
MaheshKamuju <54261609+MaheshKamuju@users.noreply.github.com>
2024-09-12 14:52:20 +0530

    Delete dockerinstall.sh

--- dockerinstall.sh
+++
@@ -1,7 +0,0 @@
-sudo yum update -y
-sudo amazon-linux-extras install docker
-sudo yum install -y docker
-sudo service docker start
-sudo systemctl enable docker
-sudo usermod -a -G docker ec2-user
-sudo usermod -a -G docker jenkins
